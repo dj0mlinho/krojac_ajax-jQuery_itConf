9d8697645fbbaddbb159c473661890178605588e
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2018-12-19 11:48:34 +0100

    uradio na svoj način - štucne prvi put kad se prebaci tab

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
-$.ajax({
-    url: 'https://mysafeinfo.com/api/data?list=bestnovels7&format=json&abbreviate=false&case=default',
-    type: "get",
-    dataType: "JSON"
-  })
-  .done(function (res) {
-    res.forEach(function (e) {
-      $('body').append(e.Title + "<br>")
-    })
-  })
-  .fail(function () {
-    console.log("greška")
-  })
-  .always(function () {
-    console.log("always");
-  })
+// //PREUZIMANJE INFO I AUTOMATSKO PREBACIVANJE U OBJEKTE I ISPISIVANJE U DOM-u
+
+// $.ajax({
+//     url: 'https://mysafeinfo.com/api/data?list=bestnovels7&format=json&abbreviate=false&case=default',
+//     type: "get",
+//     dataType: "JSON"
+//   })
+//   .done(function (res) {
+//     res.forEach(function (e) {
+//       $('body').append(e.Title + "<br>")
+//     })
+//   })
+//   .fail(function () {
+//     console.log("greška")
+//   })
+//   .always(function () {
+//     console.log("always");
+//   })
+$("#l1").on("click", function(e) {
+  $("li>a")[0].className = "nav-link active";
+  $("li>a")[1].className = "nav-link";
+  $("li>a")[2].className = "nav-link";
+  $("li>a")[3].className = "nav-link";
+  $(".content").load("data.html #js");
+});
+$("#l2").on("click", function(e) {
+  $("li>a")[0].className = "nav-link";
+  $("li>a")[1].className = "nav-link active";
+  $("li>a")[2].className = "nav-link";
+  $("li>a")[3].className = "nav-link";
+  $(".content").load("data.html #php");
+});
+$("#l3").on("click", function(e) {
+  $(".content").load("data.html #java");
+  $("li>a")[0].className = "nav-link";
+  $("li>a")[1].className = "nav-link";
+  $("li>a")[2].className = "nav-link active";
+  $("li>a")[3].className = "nav-link";
+});
+$("#l4").on("click", function(e) {
+  $(".content").load("data.html #wp");
+  $("li>a")[0].className = "nav-link";
+  $("li>a")[1].className = "nav-link";
+  $("li>a")[2].className = "nav-link";
+  $("li>a")[3].className = "nav-link active";
+});
